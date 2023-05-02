c4275da60bb72b28cf307765058f4bd8426ac85a
Dhiali <231299@virtualwindow.co.za>
2023-05-02 13:55:18 +0200

    week 2.2

---
+++ js/index.js
@@ -0,0 +1,88 @@
+console.log("hello world")
+
+//globally scoped vars
+var firstName = "dhiali";
+var surname = "chetty";
+var age = 18;
+const subject = "development";
+
+console.log(firstName);
+console.log(surname);
+console.log(subject);
+console.log(age);
+
+let greeter = "Hello" + " " + ", my name is " + firstName + " " + surname + ", I am " + age + " years old and i am studying " + subject;
+console.log(greeter);
+
+
+let numOne= 12; 
+let numTwo = 20;
+let numThree= 10;
+let numFour= 2;
+let answer = 0;
+
+console.log(numOne);
+console.log(numTwo);
+console.log(numThree);
+console.log(numFour);
+console.log(answer);
+
+let formula = numOne + numTwo / numThree * numFour;
+console.log(formula);
+
+
+let timmy = 10;
+let timmyOne = 15;
+let timmyTwo = 100;
+
+console.log(timmyOne);
+console.log(timmyTwo);
+console.log(timmy);
+
+let timmyFormula = timmyOne / timmyTwo;
+console.log(timmyFormula);
+let timmyFormula2 = timmyFormula * timmy;
+console.log(timmyFormula2);
+
+let susan = 25;
+let susanOne = 26;
+let susanTwo = 100;
+
+console.log(susanOne);
+console.log(susanTwo);
+console.log(susan);
+
+let susanFormula = susanOne / susanTwo;
+console.log(susanFormula);
+let susanFormula2 = susanFormula * susan;
+console.log(susanFormula2);
+
+let tax = timmyFormula2 + susanFormula2;
+console.log(tax);
+
+let taxAnswer = "The tax man will claim " + "R" + tax + " from timmy and susan";
+console.log(taxAnswer);
+
+
+
+let john = 50; 
+let tim = 20;
+let alfie = 78;
+let gwen = 40;
+let test = 100;
+let number = 4; 
+
+console.log(john);
+console.log(tim);
+console.log(alfie);
+console.log(gwen);
+console.log(test);
+console.log(number);
+
+let average = john + tim + alfie + gwen ;
+console.log(average);
+let average2 = average / number ;
+console.log(average2)
+
+let averageAnswer = "The student average is:  " + average2 + " /100";
+console.log(averageAnswer);
